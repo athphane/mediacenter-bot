76398a747d68088f7b9ac229ed1a8109e3357c72
athphane <athphane@gmail.com>
2019-10-29 21:33:56 +0500

    - main categories command and get list of categories.

--- mediacenter/plugins/qbittorrent/categories.py
+++ mediacenter/plugins/qbittorrent/categories.py
@@ -1,25 +1,49 @@
 from mediacenter.plugins.admin.help import add_command_help
 from mediacenter.mediacenterbot import MediaCenterBot
-from pyrogram import Message
+from pyrogram import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import errors as pyro_errors
 from mediacenter.utilities.custom_filters import CustomFilters
 from mediacenter.utilities.Qbittorrent import TorrentClient as QBT
-from mediacenter.utilities.helpers import split_list
+# from mediacenter.utilities.helpers import split_list
 
 
 @MediaCenterBot.on_message(CustomFilters.command("categories"))
-async def categories_menu(bot: MediaCenterBot, message: Message):
+async def categories(bot: MediaCenterBot, message: Message):
+    await message.reply(
+        "Categories\nSelect an action.",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("List", 'list_categories'), InlineKeyboardButton("Create", "create_categories")],
+            ]
+        )
+    )
+
+
+@MediaCenterBot.on_callback_query(CustomFilters.callback_query('list_categories', False))
+async def list_categories(client: MediaCenterBot, callback: CallbackQuery):
     categories: dict = QBT().all_categories()
-    keys = categories.keys()
+
     categories_list = []
-    for x in keys:
+    for x in categories.keys():
         categories_list.append(categories.get(x).get('name'))
 
-    def make_buttons():
-        button_list = split_list(categories_list, 3)
-        print(button_list)
-        return button_list
+    categories_list = "\n".join(categories_list)
 
-    await message.reply(str(make_buttons()))
+    full_text = (
+        "Here are all of your categories\n"
+        "{categories}"
+    )
+
+    await callback.answer("Listing Categories")
+
+    try:
+        await callback.edit_message_text(
+            full_text.format(categories=categories_list),
+            reply_markup=callback.message.reply_markup
+        )
+    except pyro_errors.MessageNotModified:
+        pass
+
 
 # Command help section
 add_command_help(
