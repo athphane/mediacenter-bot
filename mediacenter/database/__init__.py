56eb047301eddbefd4eb949e4cdb4d3ed4dc2896
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-01 21:54:12 +0500

    - Started on database queries.

---
+++ mediacenter/database/__init__.py
@@ -0,0 +1,14 @@
+import pymongo
+from mediacenter import MONGO_URL, DB_USERNAME, DB_PASSWORD, DB_NAME
+
+
+def database():
+    """Created Database connection"""
+    client = pymongo.MongoClient(
+        MONGO_URL,
+        username=DB_USERNAME,
+        password=DB_PASSWORD
+    )
+    db = client[DB_NAME]
+    return db
+
---
+++ mediacenter/database/qbittorrent.py
@@ -0,0 +1,1 @@
+# TODO: Implement Database queries for qbittorrent notification system..
