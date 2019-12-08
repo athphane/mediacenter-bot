e7a360fab8da78a71782e2799e7e21ce4b87dddd
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-08 11:24:50 +0500

    - moved out beykaaru things.

--- mediacenter/__main__.py
+++ mediacenter/__main__.py
@@ -1,22 +1,13 @@
 from mediacenter.mediacenterbot import MediaCenterBot
-from mediacenter import scheduler, ADMIN
-from mediacenter.scheduler_system.create_jobs import add_job
+from mediacenter import scheduler
 import mediacenter
-
-
-async def test_function(client: MediaCenterBot):
-    await client.send_message(ADMIN, "something")
 
 
 if __name__ == '__main__':
     app = MediaCenterBot()
 
-    # Function to add all scheduled jobs to scheduler
+    # Assign app to variable in __init__
     mediacenter.client = app
 
-    add_job(test_function)
-
-    # Start scheduled jobs
     scheduler.start()
-
     app.run()
