3d1248d3ce0168f92e543cda568493b83c97b214
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-08 11:24:28 +0500

    - i know the last commit MESSAGE had a spelling mistake

--- mediacenter/scheduler_system/create_jobs.py
+++ mediacenter/scheduler_system/create_jobs.py
@@ -3,10 +3,7 @@ from mediacenter import scheduler
 
 
 def set_client(new_client):
-    print(mediacenter.client)
     mediacenter.client = new_client
-    print("from the setter")
-    print(mediacenter.client)
 
 
 def add_job(function):
