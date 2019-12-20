4716808ef7ea7fb980fca7e1657257249c7d19b2
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-20 23:48:31 +0500

    - started incident commands

--- mediacenter/plugins/admin/incidents.py
+++ mediacenter/plugins/admin/incidents.py
@@ -1,13 +1,15 @@
-from mediacenter import ALLOWED_USERS
 from mediacenter.mediacenterbot import MediaCenterBot
-from pyrogram import Message, Filters
+from pyrogram import Message
 from mediacenter.plugins.admin.help import add_command_help
 from mediacenter.utils.custom_filters import CustomFilters
+from mediacenter.database.incidents import Incident
 
 
 @MediaCenterBot.on_message(CustomFilters.command("lastIncident", case_sensitive=True))
 async def start(bot: MediaCenterBot, message: Message):
-    await message.reply("Welcome to TorrentBot! I help you control your media center.")
+    last_incident = Incident().find_latest_incident()
+    print(last_incident)
+    # TODO: FIX THIS SHIT
 
 
 # Command help section
