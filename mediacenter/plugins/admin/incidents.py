93576acdac0e3e1cf9f05427559ea56212a4d072
MohamedAthfanKhaleel <athphane@gmail.com>
2019-12-20 23:29:43 +0500

    - case sensitivity to match help message

--- mediacenter/plugins/admin/incidents.py
+++ mediacenter/plugins/admin/incidents.py
@@ -5,7 +5,7 @@ from mediacenter.plugins.admin.help import add_command_help
 from mediacenter.utils.custom_filters import CustomFilters
 
 
-@MediaCenterBot.on_message(CustomFilters.command("incidents"))
+@MediaCenterBot.on_message(CustomFilters.command("lastIncident", case_sensitive=True))
 async def start(bot: MediaCenterBot, message: Message):
     await message.reply("Welcome to TorrentBot! I help you control your media center.")
 
